97add41baeab1eec352ebac0dae0ddeb59985915
Parnika Singla <parnikasingla@gmail.com>
2013-05-01 12:48:21 +0530

    file added

--- models/contacts.js
+++ models/contacts.js
@@ -3,19 +3,62 @@
 var db = require('./db');
 
 var contacts = {
-  create: function(data, cb) {
-    // insert data into db
-
-    cb (err || null, result);    
+  create: function (data, cb) {
+    var required = ['name', 'email', 'phone'];
+    var i = 0;
+    for (i; i < required.length; i++) {
+      if (!data[required[i]]) {
+        return cb(new Error('missing: ' + required[i]));
+      }
+    }
+    var query = 'INSERT INTO contacts SET ?';
+    db.query(query, data, cb);
   },
 
-  list: function(cb) {
-    // fetch contacts
-    // return via cb
-    // cb (err, results);
-    cb(null, ['a', 'b']);
+  get: function (id, cb) {
+    var query = "SELECT * FROM contacts where id=?";
+    db.query(query, [id], cb);
+  },
+
+  by_email: function(email, cb) {
+    var query = "SELECT * FROM contacts where email=?";
+    db.query(query, [email], cb);
+  },
+
+  by_phone: function(phone, cb) {
+    var query = "SELECT * FROM contacts where phone=?";
+    db.query(query, [phone], cb);
+  },
+
+  list: function (cb) {
+    var query = "SELECT * FROM contacts";
+    db.query(query, cb);
   }
 
 }
 
 module.exports = contacts;
+
+if (require.main === module) {
+  (function () {
+    var newcontact = {
+      name: 'Parnika Singla',
+      email: 'parnikasingla@gmail.com',
+      phone: '8826724555'
+    };
+    contacts.create(newcontact, function (err, res) {
+      if (!err && res) {
+        var id = res.insertId;
+        contacts.get(id, function (err, contact) {
+          console.log(err || contact);
+          contacts.list(function (err, result) {
+            console.log(err || result);
+            process.exit(1);
+          });          
+        });
+      } else { 
+        console.log(err || new Error('no response received'));
+      }
+    })
+  })();
+}
