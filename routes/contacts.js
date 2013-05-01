42b89f3c1868846839ba5298d7297770b120b110
Parnika Singla <parnikasingla@gmail.com>
2013-05-01 15:58:02 +0530

    file

--- routes/contacts.js
+++ routes/contacts.js
@@ -18,14 +18,42 @@ var contacts = {
 
   validate: function (req, res, next) {
     // server side validation
- 
+
+   var data = {
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone
+    };
+   //req.check('email', 'Please enter a valid email').len(6,64).isEmail();
+   console.log(data.name);
+   var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+var filter1 = /^[a-zA-Z0-9]*$/;
+var filter2 = /^[\d]*$/;
+
+if((filter.test(data.email)) &&  (filter1.test(data.name)) && (filter2.test(data.phone)))
+{
+  return true;
+}
+else{
+  var str="";
+ if ((filter.test(data.email)) == false) 
+      str='enter valid email id';
+     
+ if((filter1.test(data.name)) == false)
+      str='enter valid name';
+
+ if((filter2.test(data.phone)) == false)
+      str='enter valid phone number';
+}
+  
 
     console.log(req.body);
     var err;
 
     // if validation fail set err to indicate error
 
-    next(err);
+    //next(err);
+    res.render('error',{title:'title',asdf:str});
   },
 
   create: function (req, res, next) {
