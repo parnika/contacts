b664d7e3c90f5d25e97842ea874713efde1ff9df
Parnika Singla <parnikasingla@gmail.com>
2013-05-07 15:50:57 +0530

    bug fixes

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,99 +3,94 @@
 var contactsModel = require('../models/contacts');
 
 var contacts = {
-  
-  list: function (req, res, next) {
-    contactsModel.list(function (err, list) {
+
+  list: function(req, res, next) {
+    contactsModel.list(function(err, list) {
       req.contact_list = list;
       next(err);
     });
   },
 
-  details: function(req,res,next){
-    contactsModel.getNames(function (err, list) {
-      res.render('details',{title:'details',value:list});
+  details: function(req, res, next) {
+    contactsModel.getNames(function(err, list) {
+      res.render('details', {
+        title: 'details',
+        value: list
       });
-    
+    });
+
   },
 
-  deleteRecord: function(req,res,next){
-   var name = req.body.name;
-    console.log('name'+name);
-    contactsModel.delByName(name, function(err,list){
-    res.render('deleted',{title:'deleted record',affectedrow:list.affectedRows});
+  deleteRecord: function(req, res, next) {
+    var name = req.body.name;
+    console.log('name' + name);
+    contactsModel.delByName(name, function(err, list) {
+      res.render('deleted', {
+        title: 'deleted record',
+        affectedrow: list.affectedRows
+      });
     });
   },
 
 
 
-  showDeleteForm: function(req,res,next){
-      res.render('deletion',{title:'delete record'});
+  showDeleteForm: function(req, res, next) {
+    res.render('deletion', {
+      title: 'delete record'
+    });
   },
 
-  showSearchForm: function(req,res,next){
-       res.render('searched',{title:'searching'});
+  showSearchForm: function(req, res, next) {
+    res.render('searched', {
+      title: 'searching'
+    });
   },
 
-  searchRecord: function(req,res,next){
-    var name= req.body.name;
-      //email: req.body.email;
-      //phone: req.body.phone;
-      console.log('searched name:'+name);
-      contactsModel.by_name(name, function(err,list){
+  searchRecord: function(req, res, next) {
+    var name = req.body.name;
+    //email: req.body.email;
+    //phone: req.body.phone;
+    console.log('searched name:' + name);
+    contactsModel.by_name(name, function(err, list) {
       //console.log(JSON.stringify(list, null, 2));
       console.log(JSON.stringify(list));
-        res.render('searchh',{title:'searched record',det:list});
+      res.render('searchh', {
+        title: 'searched record',
+        det: list
       });
+    });
   },
 
-  showform: function (err, res, next) {
+  showform: function(err, res, next) {
     res.render('show-form', {
       title: 'Create new contact'
     });
   },
 
-  validate: function (req, res, next) {
+  validate: function(req, res, next) {
     // server side validation
 
-   var data = {
+    var data = {
       name: req.body.name,
-      email: re.body.email,
+      email: req.body.email,
       phone: req.body.phone
     };
-   
-   console.log(data.name);
-var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-var filter1 = /^[a-zA-Z0-9]*$/;
-var filter2 = /^[\d]*$/;
+    console.log('in validate', data);
+    var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    var filter1 = /^[a-zA-Z0-9]*$/;
+    var filter2 = /^[\d]*$/;
 
-if((filter.test(data.email)) &&  (filter1.test(data.name)) && (filter2.test(data.phone)))
-{
-  return true;
-}
-else{
-  var str="";
- if ((filter.test(data.email)) == false) 
-      str='enter valid email id';
-     
- if((filter1.test(data.name)) == false)
-      str='enter valid name';
-
- if((filter2.test(data.phone)) == false)
-      str='enter valid phone number';
-}
-  
-
-    console.log(req.body);
-    var err;
-
-    // if validation fail set err to indicate error
-
-    //next(err);
-    //var asdf;
-    res.render('error',{title:'title',asdf:str});
+    if ((filter.test(data.email)) && (filter1.test(data.name)) && (filter2.test(data.phone))) {
+      next();
+    } else {
+      var str;
+      if (!filter.test(data.email)) str = 'enter valid email id';
+      if (!filter1.test(data.name)) str = 'enter valid name';
+      if (!filter2.test(data.phone)) str = 'enter valid phone number';
+      next(new Error(str));
+    }
   },
-
-  create: function (req, res, next) {
+  create: function(req, res, next) {
     // call model.create
 
     var data = {
@@ -103,18 +98,16 @@ else{
       email: req.body.email,
       phone: req.body.phone
     };
-
     contactsModel.create(data, function(err, result) {
-      console.log(err || result);
       next(err);
     });
   },
 
-  createResponse: function (req, res, next) {
-    res.send('contact added');
+  createResponse: function(req, res, next) {
+    res.redirect('/');
   },
 
-  
+
 }
 
 module.exports = contacts;
